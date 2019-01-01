a02a7e456f5e902d7b7c9d8d5a499e7346af0b8f
rudolf <rudolfss13@gmail.com>
2019-01-01 19:45:58 +0300

    Config fix.

--- src/main/kotlin/com/github/rudolf/arduino/task/Config.kt
+++ src/main/kotlin/com/github/rudolf/arduino/task/Config.kt
@@ -6,7 +6,7 @@ fun generateConfig(workingDir: File, additionalBoards: List<String>) =
 """
 board_manager:
     additional_urls:
-        ${additionalBoards.map { "- $it" }.joinToString("\n")}
+${additionalBoards.map { "        - $it" }.joinToString("\n")}
 proxy_type: auto
 sketchbook_path: ${workingDir.absolutePath}
 arduino_data: ${workingDir.absolutePath}
